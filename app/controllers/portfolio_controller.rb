9fab035b613203e1e9e6a9c35541f9ed566fc341
Travis Eubanks <tmeubanks113@gmail.com>
2015-07-08 09:11:43 -0500

    bundled with treehouse api call

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,7 +1,15 @@
+require 'open-uri'
+require 'json'
+
 class PortfolioController < ApplicationController
+  def home
+    url = "https://teamtreehouse.com/traviseubanks.json"
 
-def home
-end
+    open_url = open(url).read
+    json_parsed_data = JSON.parse(open_url)
 
+    @badges = json_parsed_data['badges']
+    @points = json_parsed_data['points']
+  end
 end
 
