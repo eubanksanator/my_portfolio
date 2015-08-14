a0e609ddabda502782b5a4c5febe9ef98f1287d3
Travis Eubanks <tmeubanks113@gmail.com>
2015-08-14 12:59:16 -0500

    adjusts portfolio controller

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -2,14 +2,14 @@ require 'open-uri'
 require 'json'
 
 class PortfolioController < ApplicationController
-  def home
-    url = "https://teamtreehouse.com/traviseubanks.json"
+  # def home
+  #   url = "https://teamtreehouse.com/traviseubanks.json"
 
-    open_url = open(url).read
-    json_parsed_data = JSON.parse(open_url)
+  #   open_url = open(url).read
+  #   json_parsed_data = JSON.parse(open_url)
 
-    @badges = json_parsed_data['badges']
-    @points = json_parsed_data['points']
-  end
+  #   @badges = json_parsed_data['badges']
+  #   @points = json_parsed_data['points']
+  # end
 end
 
